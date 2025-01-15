25108af4a47250abc14e540ff2abcc29e72dc458
Szymon <Szymon.Zilinski@grassvalley.com>
2025-01-15 03:17:47 +0100

    trying to allow cors in the code

--- AzureFunctions/GetTradingHistory.cs
+++ AzureFunctions/GetTradingHistory.cs
@@ -6,17 +6,38 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace AzureFunctions
 {
     public static class GetTradingHistory
     {
+        private static readonly string[] AllowedOrigins = new string[]
+        {
+            "https://127.0.0.1:4200",
+            "https://simon1pl.github.io"
+        };
+
         [FunctionName("TradingHistory")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "options", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (req.Method == "OPTIONS")
+            {
+                string origin = req.Headers["Origin"];
+                if (AllowedOrigins.Contains(origin))
+                {
+                    req.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", origin);
+                }
+
+                req.HttpContext.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+                req.HttpContext.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+                req.HttpContext.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+                return new OkResult();
+            }
 
             string name = req.Query["name"];
 
